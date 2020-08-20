b432626b391d06a4671d9db3ae05621b4524bd28
HwnaukLee <hwanuk.lee@gmail.com>
2020-08-20 16:22:14 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -53,6 +53,24 @@ const customers = [
 ]
 
 class App extends Component{
+
+  state ={
+    customers: ""
+
+  }
+
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err) );
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
    render(){
      const { classes } = this.props;
 
@@ -69,7 +87,9 @@ class App extends Component{
              </TableRow>
            </TableHead>
            <TableBody>
-            {customers.map ( c => { return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} jog={c.job} /> ); }) }
+            {this.state.customers ? this.state.customers.map ( 
+              c => { return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} jog={c.job} /> ); 
+            }) : "" }
            </TableBody>
          </Table>
        </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,38 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
+/* app.get('/api/hello', (req, res) => {
     res.send({message: 'Hello Express!'});
+}); */
+
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '이예송',
+            'birthday': '1973',
+            'gender': '남자',
+            'job': '직장인'
+          },
+          {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '이지백',
+            'birthday': '1973',
+            'gender': '남자',
+            'job': '디자이너'
+          },
+          {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '이대율',
+            'birthday': '1973',
+            'gender': '남자',
+            'job': '개발자'
+          }
+    ]);
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
